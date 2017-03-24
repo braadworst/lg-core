c88d7cb4ad5b6101424bc124e9d67fb5dc26ddb5
braadworst <royniels83@gmail.com>
2017-03-24 13:34:26 +0800

    Updated the type checks to check-types

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
+const check = require('check-types');
+
 module.exports = () => {
   let selected, environments = {}, middlewares = {}, extensions = {}, listeners = {};
 
   function update(options, ...parameters) {
-    shouldExist(options, 'environments');
-    shouldExist(options, 'method');
-    shouldExist(exposed, options.method);
-    shouldExist(options, 'path');
-    isString(options.method);
-    isString(options.path);
-    selectedEnvironments.forEach(isString);
-    options.environments.forEach(environment => shouldExist(environments, environment));
-    options.environments.forEach(environment => {
-      const middleware = environments[environment].middleware;
-      const names = name[0] === '/' ? environments[environment].pathsByPath[options.path] : [options.path];
-      const stack = middleware
-        .filter(record => listeners[options.method].indexOf(record.method) > -1)
-        .filter(record => record.names.filter(name => names.indexOf(name) > -1).length > 0);
+    check.assert.assigned(options.environments, 'Please provide environment names');
+    check.assert.assigned(options.method, 'Please provide a method name');
+    check.assert.assigned(options.path, 'Please provide a path name');
+    check.assert.nonEmptyString(options.method, 'Method needs to be of type string');
+    check.assert.nonEmptyString(options.path, 'Path needs to be of type string');
+    check.assert.hasLength(options.environments, 1, 'Provide at least one environment name');
+    check.assert.array.of.nonEmptyString(options.environments, 'All supplied environment names need to be a string');
+    if (exposed[options.method]) {
+      options.environments.forEach(environment => {
+        if (environments[environment]) {
+          const middleware = environments[environment].middleware;
+          const names = name[0] === '/' ? environments[environment].pathsByPath[options.path] : [options.path];
+          const stack = middleware
+            .filter(record => listeners[options.method].indexOf(record.method) > -1)
+            .filter(record => record.names.filter(name => names.indexOf(name) > -1).length > 0);
 
-      let relay = {};
-      function thunkify(middleware) {
-        if (!middleware) { return function last() {}; }
-        if (typeof middleware.callback !== 'function') {
-          throw new Error('Middleware needs to be a function');
-        }
-        return function(defined) {
-          isObject(defined);
-          relay = Object.assign({}, relay, defined)
-          middleware.callback(
-            thunkify(stack.shift()),
-            relay,
-            ...parameters
-          );
-        }
-      }
+          let relay = {};
+          function thunkify(middleware) {
+            if (!middleware) { return function last() {}; }
+            if (typeof middleware.callback !== 'function') {
+              throw new Error('Middleware needs to be a function');
+            }
+            return function(defined) {
+              isObject(defined);
+              relay = Object.assign({}, relay, defined)
+              middleware.callback(
+                thunkify(stack.shift()),
+                relay,
+                ...parameters
+              );
+            }
+          }
 
-      thunkify(stack.shift())(extensions);
-    });
+          thunkify(stack.shift())(extensions);
+        }
+      });
+    }
   }
 
   function loop(callback) {
@@ -44,45 +49,10 @@ module.exports = () => {
     });
   }
 
-  function isString(value) {
-    if (typeof value !== 'string') {
-      throw new Error(`Supplied argument needs to be a string, got type ${ typeof value }, ${ value }`);
-    }
-    if (value.length === 0) {
-      throw new Error('Supplied argument is an empty string');
-    }
-    return value;
-  }
-
-  function hasLength(array, length = 1) {
-    if (array.length < length) {
-      throw new Error('Please supply all needed arguments');
-    }
-    return array;
-  }
-
-  function shouldExist(object, key) {
-    if (!object[key]) {
-      throw new Error(`${ key } has not been defined`);
-    }
-  }
-
-  function shouldNotExist(object, key) {
-    if (object[key]) {
-      throw new Error(`${ key } has already been defined`);
-    }
-  }
-
-  function isObject(value) {
-    if (typeof value !== 'object') {
-      throw new Error('Supplied value should be of type object');
-    }
-  }
-
   const exposed = {
     environment(name) {
-      shouldNotExist(environments, name);
-      isString(name);
+      check.assert.not.assigned(environments[name], `${ name } has already be defined as an environment`);
+      check.assert.nonEmptyString(name, 'Provided environment needs to be a string');
       environments[name] = {
         middleware  : [],
         pathsByName : {},
@@ -91,46 +61,56 @@ module.exports = () => {
       return exposed;
     },
     extension(name, extension) {
-      isString(name);
+      check.assert.not.assigned(extensions[name], `${ name } has already be defined as an extension`);
+      check.assert.nonEmptyString(name, 'Provided extension name needs to be a string');
       extensions[name] = extension;
       return exposed;
     },
-    middleware(defined = {}) {
-      isObject(defined);
-      Object.keys(defined).forEach(key => shouldNotExist(middlewares, key));
+    middleware(defined) {
+      check.assert.nonEmptyObject(name, 'Provided middleware needs to be a non empty object');
+      Object.keys(defined).forEach(key => {
+        check.assert.not.assigned(extensions[name], `${ name } has already be defined as an extension`);
+      });
       middlewares = Object.assign({}, middlewares, defined);
     },
     listener(method, ...listeners) {
-      isString(method);
-      hasLength(listeners).forEach(isString).forEach(method => shouldExist(exposed, method));
-      shouldExist(listeners, method);
+      check.assert.nonEmptyString(method, 'Provided listener method needs to be a string');
+      check.assert.hasLength(listeners, 1, 'Please add at least one listener method');
+      check.assert.array.of.nonEmptyString(listeners, 'All listener methods need to be a string');
+      check.assert.assigned(exposed[method], `${ method } has not been defined as an event method`);
+      listeners.forEach(listener => {
+        check.assert.assigned(exposed[listener], `${ listener } has not been defined as an event method`);
+      });
       listeners[method] = [...listeners[method], ...listeners];
       return exposed;
     },
     events(extension, ...methods) {
-      isString(extension);
-      hasLength(methods).forEach(isString);
+      check.assert.nonEmptyString(extension, 'Provided extension needs to be a string');
+      check.assert.hasLength(methods, 1, 'Please add at least one event method name');
+      check.assert.array.of.nonEmptyString(methods, 'All event method names need to be a string');
       if (extensions[extension] && typeof extensions[extension] === 'function') {
         extensions[extension](update, { environments : selected });
       }
       methods.forEach(method => {
-        shouldNotExist(exposed, method);
+        check.assert.not.assigned(exposed[method], `${ method } has already been defined as an event method`);
         listeners[method] = [method];
         exposed[method] = (name, middleware, ...parameters) => {
-          let names;
-          if (name[0] === '-') {
-            name = name.slice(1);
-            shouldExist(environment.pathsByName, name);
-            names = Object.clone({}, environment.pathsByName);
-            delete names[name];
-            names = Object.keys(names).filter(key => names[key].length === 1).map(key => names[key]);
-          } else {
-            shouldExist(environment.pathsByName, name);
-            names = [name];
-          }
-
+          check.assert.nonEmptyString(name, 'Provided path alias needs to be a string');
+          check.assert.nonEmptyString(middleware, 'Provided middleware name needs to be a string');
+          check.assert.not.equal(name, '-' `Path alias cannot be "-"`);
           loop(environment => {
             if (middlewares[middleware]) {
+              let names;
+              if (name[0] === '-') {
+                name = name.slice(1);
+                check.assert.assigned(environment.pathsByName[name], `${ name } has not been defined as a path alias`);
+                names = Object.clone({}, environment.pathsByName);
+                delete names[name];
+                names = Object.keys(names).filter(key => names[key].length === 1).map(key => names[key]);
+              } else {
+                check.assert.assigned(environment.pathsByName[name], `${ name } has not been defined as a path alias`);
+                names = [name];
+              }
               let callback = middlewares[middleware];
               callback = parameters ? callback(parameters) : callback;
               environment.middleware.push({ method, names, callback });
@@ -138,40 +118,46 @@ module.exports = () => {
           });
         }
       });
-
       return exposed;
     },
     where(...defined) {
-      hasLength(defined).forEach(isString);
+      check.assert.hasLength(defined, 1, 'Please provide at least one environment name');
+      check.assert.array.of.nonEmptyString(defined, 'All supplied environment names need to be a string');
+      defined.forEach(environment => {
+        check.assert.assigned(environments[environment], `${ environment } has not been defined as a environment name`);
+      });
       selected = defined;
       return exposed;
     },
     path(name, path) {
-      isString(name);
-      isString(path);
+      check.assert.nonEmptyString(name, 'Provided path alias needs to be a string');
+      check.assert.nonEmptyString(path, 'Provided path needs to be a string');
+      check.assert.not.equal(name, '-', 'Path alias cannot be "-"');
       loop(environment => {
-        shouldNotExist(environment.pathsByPath, path);
-        shouldNotExist(environment.pathsByName, name);
+        check.assert.not.assigned(environment.pathsByPath[path], `${ path } has already been defined`);
+        check.assert.not.assigned(environment.pathsByName[name], `${ name } has already been defined`);
         environment.pathsByPath[path] = [name];
         environment.pathsByName[name] = [path];
       });
       return exposed;
     },
     pathGroup(name, ...names) {
-      isString(name);
-      hasLength(names, 2).forEach(isString);
+      check.assert.nonEmptyString(name, 'Provided path group alias needs to be a string');
+      check.assert.hasLength(names, 2, 'Please provide at least two path aliases to create a path group');
+      check.assert.array.of.nonEmptyString(names, 'All supplied path aliases need to be a string');
       loop(environment => {
-        shouldNotExist(environment.pathsByName, name);
+        check.assert.not.assigned(environment.pathsByName[name], `${ name } has already been defined`);
+        names.forEach(record => {
+          check.assert.assigned(environment.pathsByName[record], `${ record } has not been defined as path alias or path group`);
+        });
         environment.pathsByName[name] = names.reduce((reduced, record) => {
           return [...reduced, ...environment.pathsByName[record]];
-        }, [])
+        }, []);
         environment.pathsByName[name].forEach(path => {
-          shouldExist(environment.pathsByPath, path);
           environment.pathsByPath[path].push(name);
         });
       });
     },
   }
-
   return exposed;
 }
